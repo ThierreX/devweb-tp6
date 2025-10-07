74a44fd1faccfabf8fe15759bba862c39a0d7abd
Thierry <tiryguo@hotmail.com>
2025-10-08 03:01:43 +1100

    Add files via upload

--- config.mjs
+++ config.mjs
@@ -6,5 +6,7 @@ export default {
   linkLen: process.env.LINK_LEN ? Number(process.env.LINK_LEN) : 6,
   dbFile: process.env.DB_FILE || "database/database.sqlite",
   dbSchema: process.env.DB_SCHEMA || "database/database.sql",
-  baseUrl: process.env.BASE_URL || `http://localhost:${process.env.PORT || 8080}`
+  baseUrl: process.env.BASE_URL 
+        || process.env.RENDER_EXTERNAL_URL 
+        || `http://localhost:${process.env.PORT || 8080}`
 };
